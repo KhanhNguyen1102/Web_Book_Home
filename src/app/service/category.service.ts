93436cc790485a2324d9374c6d3ef1e725b94bfe
Your Name <you@example.com>
2022-01-22 17:14:47 +0700

    off chieu 22/1

--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Category} from "../model/category";
+import {Home} from "../model/home";
 const api_url='http://localhost:8080/api/homes/'
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,8 @@ export class CategoryService {
   constructor(private http :HttpClient) { }
   getAll() :Observable<Category[]>{
     return this.http.get<Category[]>(api_url+'findAllCategory')
+  }
+  findOne():Observable<Home[]>{
+    return this.http.get<Home[]>(api_url)
   }
 }
