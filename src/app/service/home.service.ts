959016edf27bfadc02704a3b16e9d078c7db4d71
Your Name <you@example.com>
2022-01-23 09:16:06 +0700

    them find one in home

--- src/app/service/home.service.ts
+++ src/app/service/home.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Home} from "../model/home";
+const api_url='http://localhost:8080/api/homes'
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+  // @ts-ignore
+  findOne(id) : Observable<Home>{
+    return this.http.get<Home>(api_url+'/'+id)
+  }
 }
