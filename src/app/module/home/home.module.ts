f787aee263aad048c40d97b664fe305c3e74a80c
hoangducviet1993 <ducviet12vl@gmail.com>
2022-01-25 22:00:33 +0700

    list -home

--- src/app/module/home/home.module.ts
+++ src/app/module/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { FindOneComponent } from './find-one/find-one.component';
+import { ListHomeComponent } from './list-home/list-home.component';
 
 
 @NgModule({
   declarations: [
-    FindOneComponent
+    FindOneComponent,
+    ListHomeComponent
   ],
   imports: [
     CommonModule,
