04534dc56008851d4d0247cd9531c444073ec75c
hoangducviet1993 <ducviet12vl@gmail.com>
2022-01-25 22:24:11 +0700

    show all list home

--- src/app/module/home/home-routing.module.ts
+++ src/app/module/home/home-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CategoryListComponent} from "../category/category-list/category-list.component";
 import {FindOneComponent} from "./find-one/find-one.component";
+import {ListHomeComponent} from "./list-home/list-home.component";
 
 const routes: Routes = [
   {
     path :'findOne/:id',
     component: FindOneComponent
+  },
+  {
+    path :'findALL',
+    component: ListHomeComponent
   }
 ];
 
